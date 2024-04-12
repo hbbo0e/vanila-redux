2efe8b6d7c201f67677d2199bba978a2c991632e
hbbo0e <44goodluck44@gmail.com>
2024-04-12 20:36:12 +0900

    createStore

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { createStore } from 'redux';
+
+const plus = document.getElementById('plus');
+const minus = document.getElementById('minus');
+const number = document.querySelector("span");
+
+const reducer = (state) => {
+
+  return state;
+}
+
+const store = createStore(reducer);
